d635e041efe22c08fbdfd4f899ab39b1c501a964
R_Cantor_EnviSci <jineacantor@gmail.com>
2026-07-02 15:48:38 +0800

    Create aqi_checker.py

---
+++ aqi_checker.py
@@ -0,0 +1,7 @@
+# Air Quality Index (AQI) Checker for Taguig
+aqi_level = 120
+
+if aqi_level <= 50:
+    print("The air quality is Safe. Perfect day for field research!")
+else:
+    print("Warning: High pollution detected. Wear a protective mask.")
